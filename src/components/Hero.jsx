55b700cfc402b716e2ce1a30274ecce8e43edbe4
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-04-28 17:57:57 +0530

    Added new Resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -70,7 +70,7 @@ const Hero = () =>{
                             </motion.p>
                             <motion.a
                             variants={childVariants}
-                            href="/Amar_Kumar_Shaw_Resume_(4_YOE).pdf"
+                            href="/Amar_Kumar_Shaw_Resume(4_YOE).pdf"
                             target="_blank"
                             rel="noopener noreffer"
                             whileHover={{ scale: 1.2 }}
